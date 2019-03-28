9331fcff3a1bcd1bf337422187da68b3484e9e6f
SoraNimi <434624198@qq.com>
2019-03-28 09:30:57 +0800

    ---------添加lambda表达式和动态代理-------------

--- src/Lambda/Test4.java
+++ src/Lambda/Test4.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 //实战4： reduce函数。
+//Reduce中文含义为：减少、缩小；而Stream中的Reduce方法干的正是这样的活：
+//根据一定的规则将Stream中的元素进行计算后返回一个唯一的值。
 public class Test4 {
 
     public static void main(String[] args) {
